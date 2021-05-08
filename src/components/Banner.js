1563e219c9f2207915025e433545cf140e98901f
Barry Aguibou <aguibou32@gmail.com>
2021-05-08 15:52:42 +0200

    truncate function implemented

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,6 +2,9 @@ import React from "react";
 import "./Banner.css";
 
 function Banner() {
+  const truncate = (string, n) => {
+    return string?.length > n ? string.substr(0, n - 1) + "..." : string;
+  };
   return (
     <header
       className="banner"
@@ -16,7 +19,21 @@ function Banner() {
         <div className="banner__buttons">
           <button className="banner__button">Play</button>
           <button className="banner__button">My List</button>
-          <h1 className="banner__description">This is a test description</h1>
+          <h1 className="banner__description">
+            {truncate(
+              ` Lorem ipsum dolor sit amet consectetur adipisicing elit. Sit
+            molestias autem vitae excepturi, illo id beatae rem reprehenderit ad
+            maxime veritatis, ex aut, similique ipsam qui cum sint corrupti eum.
+            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptatum
+            consequatur maiores ea voluptate in perferendis, illum asperiores
+            eum necessitatibus nesciunt, nobis doloribus voluptatibus quo. Neque
+            sunt quisquam delectus velit minima. Lorem, ipsum dolor sit amet
+            consectetur adipisicing elit. Consectetur quisquam reiciendis est
+            totam animi, rerum minus! Autem animi, blanditiis velit aliquam enim
+            sunt fuga consequatur. Harum officiis totam doloremque quae.`,
+              150
+            )}
+          </h1>
         </div>
       </div>
 
